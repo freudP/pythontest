8c4d7ca6b8a4a7408733c9f334e421a589c40df8
Flint <panzhg@sina.cn>
2016-03-21 18:54:57 +0800

    office

--- funcation.py
+++ funcation.py
@@ -115,10 +115,73 @@ print(calc())
 #已经有一个list或者tuple，要调用一个可变参数.Python允许你在list或tuple前面加一个*号，把list或tuple的元素变成可变参数传进去
 nums = [1, 2, 3]
 print(calc(nums[0],nums[1],nums[2]))
+#Python允许你在list或tuple前面加一个*号，把list或tuple的元素变成可变参数传进去
 print(calc(*nums))
 print('\n')
 
 #关键字参数,关键字参数允许你传入0个或任意个含参数名的参数，这些关键字参数在函数内部自动组装为一个dict关键字参数允许你传入0个或任意个含参数名的参数，这些关键字参数在函数内部自动组装为一个dict。
 def person(name, age, **kw):
-    print('name:', name, 'age:', age, 'other:', kw)
+    return('name:', name, 'age:', age, 'other:', kw)
 
+print(person('Adam', 45, gender='M', job='Engineer'))
+
+
+def person(name, age, **kw):
+    if 'city' in kw:
+        # 有city参数
+        pass
+    if 'job' in kw:
+        # 有job参数
+        pass
+    return('name:', name, 'age:', age, 'other:', kw)
+
+print( person('Jack', 24, city='Beijing', addr='Chaoyang', zipcode=123456))
+
+#**extra表示把extra这个dict的所有key-value用关键字参数传入到函数的**kw参数，kw将获得一个dict，注意kw获得的dict是extra的一份拷贝，对kw的改动不会影响到函数外的extra。
+extra = {'city': 'Beijing', 'job': 'Engineer'}
+print(person('Jack', 24, **extra))
+
+#限制关键字参数的名字，就可以用命名关键字参数，例如，只接收city和job作为关键字参数,和关键字参数**kw不同，命名关键字参数需要一个特殊分隔符*，*后面的参数被视为命名关键字参数。
+#使用命名关键字参数时，要特别注意，*不是参数，而是特殊分隔符。如果缺少*，Python解释器将无法识别位置参数和命名关键字参数
+def person(name, age, *, city, job):
+    return (name, age, city, job)
+print(person('Jack', 24, city='Beijing', job='Engineer'))
+#命名关键字参数必须传入参数名，这和位置参数不同。如果没有传入参数名，调用将报错
+#print(person('Jack', 24, 'Beijing', 'Engineer'))
+
+#参数组合
+#在Python中定义函数，可以用必选参数、默认参数、可变参数、关键字参数和命名关键字参数，这5种参数都可以组合使用，除了可变参数无法和命名关键字参数混合。但是请注意，参数定义的顺序必须是：必选参数、默认参数、可变参数/命名关键字参数和关键字参数
+def f1(a, b, c=0, *args, **kw):
+    return ('a =', a, 'b =', b, 'c =', c, 'args =', args, 'kw =', kw)
+
+def f2(a, b, c=0, *, d, **kw):
+    return ('a =', a, 'b =', b, 'c =', c, 'd =', d, 'kw =', kw)
+print('可变参数无法和命名关键字参数混合')
+print(f1(1,2))
+print(f1(1,2,c=3))#默认参数传值
+print(f1(1,2,3,'a','b'))#可变参数传值list or tuple
+print(f1(1,2,3,'a','b', x= 99))#关键数参数传值dict
+print(f2(1,2,d=99,ext=None))#命名关键字参数传值
+print('\n')
+print('#用list和tuple，调用函数')
+args = (1, 2, 3, 4)
+kw = {'d':99,'x':'#'}
+print(f1(*args,**kw))
+args = (1, 2, 3)
+kw = {'d': 88, 'x': '#'}
+print(f2(*args, **kw))
+
+print('''
+小结:
+Python的函数具有非常灵活的参数形态，既可以实现简单的调用，又可以传入非常复杂的参数。
+默认参数一定要用不可变对象，如果是可变对象，程序运行时会有逻辑错误！
+要注意定义可变参数和关键字参数的语法：
+*args是可变参数，args接收的是一个tuple；
+**kw是关键字参数，kw接收的是一个dict。
+以及调用函数时如何传入可变参数和关键字参数的语法：
+可变参数既可以直接传入：func(1, 2, 3)，又可以先组装list或tuple，再通过*args传入：func(*(1, 2, 3))；
+关键字参数既可以直接传入：func(a=1, b=2)，又可以先组装dict，再通过**kw传入：func(**{'a': 1, 'b': 2})。
+使用*args和**kw是Python的习惯写法，当然也可以用其他参数名，但最好使用习惯用法。
+命名的关键字参数是为了限制调用者可以传入的参数名，同时可以提供默认值。
+定义命名的关键字参数不要忘了写分隔符*，否则定义的将是位置参数。
+''')
